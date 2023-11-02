a09c979f84ab4ce454e92a7a50cd715b9e0012f0
Harvey Mackie <harveymackie@outlook.com>
2023-11-02 20:31:28 +0000

    feat: add footer and social media icons

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,6 +1,7 @@
-import { Grid, makeStyles, Typography, Box } from '@material-ui/core'
+import { Grid, makeStyles, Typography, Box, Container } from '@material-ui/core'
 import profilePicture from '../assets/profilePicture.png'
-
+import { Link } from 'react-router-dom'
+import {AiFillLinkedin, AiFillGithub, AiFillYoutube, AiFillMail} from 'react-icons/ai';
 
 const useStyles = makeStyles(theme => ({
   container: {
@@ -9,15 +10,25 @@ const useStyles = makeStyles(theme => ({
   },
   profileImage: {
     height: '15rem',
-    width: '14rem',
+    width: '13.2rem',
     borderRadius: '60%'
   },
   profileImageContainer: {
     display: 'flex',
+    flexDirection: "column",
     padding: '2rem'
   },
   aboutContainer: {
-    textAlign: 'left'
+    textAlign: 'left',
+    padding: '2rem'
+  },
+  iconContainer:{
+    padding: "2rem",
+    color: "#0c7489ff",
+    display: "flex",
+    justifyContent: "center",
+    fontSize: "1.5rem",
+    gap: "1rem"
   }
 }))
 
@@ -27,28 +38,38 @@ export default function About (props: IAboutProps) {
   const styles = useStyles()
 
   return (
-    <Grid container className={styles.container}>
-      <Grid
-        item
-        xs={12}
-        sm={6}
-        alignItems='center'
-        justifyContent='center'
-        className={styles.profileImageContainer}
-      >
-        <img className={styles.profileImage} src={profilePicture} />
-      </Grid>
-      <Grid 
-        item 
-        xs={12} 
-        sm={6} 
-        className={styles.aboutContainer}
-      >
-          <h1>Hey, I'm Harvey.</h1>
-          <h3> I'm a Software Engineer who has a <mark>passion for achieving high performance</mark> in all areas of life. </h3>
-          <p> My articles range from <b>Software Engineering</b> topics and practices to <b>Health</b> tips.</p>
-          <p> I've been balancing triathlon and my career in Software for the past two years and have managed to achieve great things in both areas of my life - <b> sub 10 hour Ironman and Colleague of the Year at Lloyds - in the same year </b>.</p>
-      </Grid>
-    </Grid>
+    <div className={styles.container}>
+      <Container>
+        <Grid container>
+          <Grid
+            item
+            xs={12}
+            sm={6}
+            alignItems='center'
+            justifyContent='center'
+            className={styles.profileImageContainer}
+          >
+            <img className={styles.profileImage} src={profilePicture} />
+            <div className={styles.iconContainer}>
+                <Link to={"https://www.linkedin.com/in/harvey-mackie/"}><AiFillLinkedin/></Link>
+                <Link to={"https://github.com/Harvey-Mackie"}><AiFillGithub/></Link>
+                <Link to={"https://www.youtube.com/channel/UCoWmEVOJiZkv7tSqjWu7Uhw"}><AiFillYoutube/></Link>
+                <Link to={"mailto:harveymackie@outlook.com"}><AiFillMail/></Link>
+              </div>
+          </Grid>
+          <Grid 
+            item 
+            xs={12} 
+            sm={6} 
+            className={styles.aboutContainer}
+          >
+              <h1>Hey, I'm Harvey.</h1>
+              <h3> I'm a Software Engineer who has a <mark>passion for achieving high performance</mark> in all areas of life. </h3>
+              <p> My articles range from <b>Software Engineering</b> topics and practices to <b>Health</b> tips.</p>
+              <p> I've been balancing triathlon and my career in Software for the past two years and have managed to achieve great things in both areas of my life - <b> sub 10 hour Ironman and Colleague of the Year at Lloyds - in the same year </b>.</p>
+          </Grid>
+        </Grid>
+      </Container>
+    </div>
   )
 }
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,28 +1,81 @@
-import * as React from 'react'
+import { Container, makeStyles } from '@material-ui/core'
+import {AiFillLinkedin, AiFillGithub, AiFillYoutube, AiFillMail} from 'react-icons/ai';
+import { Link } from 'react-router-dom';
+
+const useStyles = makeStyles(theme => ({
+  container:{
+    
+  },
+  subscribeContainer:{
+    backgroundColor: "#d7d9ceff",
+    '& > div':{
+      [theme.breakpoints.up('md')]: {
+        display: "flex",
+        '& > div':{
+          width: "50%",
+          margin: "3rem"
+        }
+      }
+    },
+  
+  },
+  subscribeContainerForm:{
+    display: "flex",
+    flexDirection: "column",
+    padding: "1rem 1rem",
+    gap: 15,
+    "& button":{
+      padding: ".5rem",
+      backgroundColor: "#119da4ff",
+      color: "white",
+      borderRadius: ".2rem"
+    },
+    "& input":{
+      padding: ".4rem",
+      borderRadius: ".2rem"
+    }
+  },
+  footerContainer:{
+    backgroundColor: "black",
+    padding: "1.5rem 12.5%",
+    color: "white",
+    display: "flex",
+    justifyContent: "center",
+    fontSize: "1.5rem",
+    gap: "1rem"
+  }
+}));
 
 export interface IFooterProps {}
 
 export default function Footer (props: IFooterProps) {
+
+  const styles = useStyles();
+
   return (
-    <div>
-      <div>
-        <div>
-          <h1>Subscribe to Weekly Snippets</h1>
-          <p>
-            Join a new community who are looking to improve in all areas of
-            life. I share actionable advise, tips, learnings and advise directly
-            to your inbox once per week.
-          </p>
-        </div>
-        <div>
-          <input type='text' name='firstName' placeholder='First Name' />
-          <input type='email' name='email' placeholder='Email Address' />
-          <button type='button'>Subscribe</button>
-        </div>
+    <div className={styles.container}>
+      <div className={styles.subscribeContainer}>
+        <Container>
+          <div>
+            <h1>Subscribe to Weekly Snippets</h1>
+            <p>
+              Join a new community who are looking to <b>improve in all areas of
+              life</b>. I share actionable advise, tips, learnings and advise directly
+              to your inbox once per week.
+            </p>
+          </div>
+          <div className={styles.subscribeContainerForm}>
+            <input type='text' name='firstName' placeholder='First Name' />
+            <input type='email' name='email' placeholder='Email Address' />
+            <button type='button'>Subscribe</button>
+          </div>
+        </Container>
       </div>
-      <div style={{ backgroundColor: 'black', color: 'white' }}>
-        <p>TRADEMARK</p>
-        <p>SOCIAL MEDIA LINKS</p>
+      <div className={styles.footerContainer}>
+        <Link to={"https://www.linkedin.com/in/harvey-mackie/"}><AiFillLinkedin/></Link>
+        <Link to={"https://github.com/Harvey-Mackie"}><AiFillGithub/></Link>
+        <Link to={"https://www.youtube.com/channel/UCoWmEVOJiZkv7tSqjWu7Uhw"}><AiFillYoutube/></Link>
+        <Link to={"mailto:harveymackie@outlook.com"}><AiFillMail/></Link>
       </div>
     </div>
   )
